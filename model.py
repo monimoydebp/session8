924f789d49e7c6cb8b4e8023eaf28bac9ef60964
monimoydebp <52818556+monimoydebp@users.noreply.github.com>
2023-06-24 22:32:48 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -106,3 +106,189 @@ class Net(nn.Module):
                
         x = x.view(-1, 10)
         return F.log_softmax(x)
+		
+		
+class Model_1(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        dropout_prob=0.1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.ReLU()
+        ) # Input=28, Output=28, rf=3
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU()
+        ) # Input=28, Output=28, rf=5
+
+        self.pool1= nn.MaxPool2d(2, 2) # Input=28, Output=14, rf=6
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU()
+        ) # Input=14, Output=14, rf=10
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU()
+        ) # Input=14, Output=14, rf=14
+
+        self.pool2= nn.MaxPool2d(2, 2) # Input=14, Output=7, rf=16
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=0),
+            nn.ReLU(),
+        ) # Input=7, Output=5, rf=24
+    
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(32, 10, 3, padding=0),
+        ) # Input=5, Output=3, rf=32
+
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # Input=3, Output=1, rf=40
+      
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.pool2(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+               
+        x = self.global_avgpool(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
+		
+class Model_2(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        dropout_prob=0.1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=28, Output=28, rf=3
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=28, Output=28, rf=5
+
+        self.pool1= nn.MaxPool2d(2, 2) # Input=28, Output=14, rf=6
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=14, Output=14, rf=10
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=14, Output=14, rf=14
+
+        self.pool2= nn.MaxPool2d(2, 2) # Input=14, Output=7, rf=16
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob) 
+        ) # Input=7, Output=5, rf=24
+
+
+       
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(14, 10, 3, padding=0, bias=False),
+        ) # Input=5, Output=3, rf=32
+
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # Input=3, Output=1, rf=40
+      
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.pool2(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+               
+        x = self.global_avgpool(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)		
+
+
+class Model_3(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        dropout_prob=0.1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=28, Output=28, rf=3
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob)
+        ) # Input=28, Output=28, rf=5
+
+        self.pool1= nn.MaxPool2d(2, 2) # Input=28, Output=14, rf=6
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob) # Input=14, Output=14, rf=10
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob) # Input=14, Output=14, rf=14
+        )
+
+        self.pool2= nn.MaxPool2d(2, 2) # Input=14, Output=7, rf=16
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(14, 14, 3, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(14),
+            nn.Dropout2d(p=dropout_prob) # Input=7, Output=5, rf=24
+        )
+
+
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(14, 10, 3, padding=0, bias=False),
+        ) # Input=5, Output=3, rf=32
+
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)  # Input=3, Output=1, rf=40
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.pool2(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+
+        x = self.global_avgpool(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
